c311b221ed7c72588669cef97dc774bce5040469
Yuri Ruas <yuri.sdor@gmail.com>
2022-12-26 19:50:20 -0300

    feat: configure mongoose connection

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://root:example@localhost:27017', {
+      dbName: 'mongodb',
+    }),
+  ],
+})
 export class AppModule { }
